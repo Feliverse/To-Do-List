ae640d080e6b2ccddf3de563790918f711c20ad9
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-01-17 23:06:48 -0400

    setting HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+        template: './src/index.html',
     }),
   ],
   output: {
