28ed2c4b8d60598fdc27acaa1527b162b7b18c53
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-01-17 23:32:42 -0400

    Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
+
     new HtmlWebpackPlugin({
         template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  
   module: {
     rules: [
       {
@@ -21,5 +26,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
