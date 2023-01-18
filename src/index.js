f4decd18b09ab5ccd8b9fc641ec8608d44f218dd
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-01-18 15:52:18 -0400

    create li in ul To Do list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  return element;
-}
+const tasks = [
+  {
+     index: 0,
+     completed: false,
+     description: 'review this project',
+  },
+  {
+     index: 1,
+     completed: true,
+     description: 'aprobe this project',
+  },
+];
 
-document.body.appendChild(component());
+const printTodoList = () => {
+  const listContainer = document.querySelector('.todo-list');
+  tasks.forEach((todo) => {
+    listContainer.innerHTML += `<li>
+    <div class="taskCont">
+      <input type="checkbox">
+      <div class="task">${todo.description}</div>
+    </div>
+    <div class="icon">&#8942</div>
+  </li>
+`;
+  });
+};
+printTodoList();
