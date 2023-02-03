31b1fc9e8faf5b34859d83a6b12fe206b68f2064
dazehinn <dazehinn.yahoo.fr>
2023-02-04 00:02:26 +0100

    Update test functions to use mock localStorage

---
+++ src/util/addDeleteTask.js
@@ -0,0 +1,33 @@
+import { fillStorage, storage } from '../test/__mocks__/fakelocalstorage';
+
+export default class Todo {
+  todoListCollection = [];
+
+  constructor() {
+    this.todoListCollection = [];
+    fillStorage();
+  }
+
+  setList = (list) => this.todoListCollection = list;
+  
+  getLists = () => this.todoListCollection;
+
+  addList = (description, completed, index) => {
+    const newList = {
+      description,
+      completed,
+      index,
+    };
+    this.todoListCollection.push(newList);
+    storage.push(newList);
+    localStorage.setItem('localStorageTodo', JSON.stringify(this.todoListCollection));
+    const li = document.createElement('li');
+    li.textContent = `${this.todoListCollection[this.todoListCollection.length - 1].description}`;
+    return li;
+  }
+
+  deleteList = (index) => {
+    const result = this.todoListCollection.filter((list, i) => i !== index);
+    this.todoListCollection = result;
+  }
+}
